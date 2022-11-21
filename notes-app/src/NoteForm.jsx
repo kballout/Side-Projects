eb155b983e9e7ae12a0eae157c349e010f899080
Kassim Ballout <54900156+kballout@users.noreply.github.com>
2022-11-21 13:07:54 -0500

    Added form ref

--- notes-app/src/NoteForm.jsx
+++ notes-app/src/NoteForm.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
-import { Col, Form, Row, Stack } from 'react-bootstrap'
+import React, { useRef } from 'react'
+import { Button, Col, Form, Row, Stack } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 import CreatableReactSelect from 'react-select/creatable'
 
-export default function NoteForm() {
+export default function NoteForm({onSubmit}) {
+    const title = useRef(null)
+    const markdown = useRef(null)
+    const [selectedTags, setSelectedTags] = useState([])
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        onSubmit({
+            title: title.current.value,
+            markdown: markdown.current.value,
+            tags: []
+        })
+    }
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
         <Stack gap={4}>
             <Row>
                 <Col>
                     <Form.Group controlId='title'>
                         <Form.Label>Title</Form.Label>
-                        <Form.Control required/>
+                        <Form.Control ref={title} required/>
                     </Form.Group>
                 </Col>
                 <Col>
@@ -24,12 +37,15 @@ export default function NoteForm() {
                 <Col>
                     <Form.Group controlId='markdown'>
                         <Form.Label>Body</Form.Label>
-                        <Form.Control required as='textarea' rows={15} />
+                        <Form.Control ref={markdown} required as='textarea' rows={15} />
                     </Form.Group>
                 </Col>
             </Row>
-            <Stack direction='horizontal'>
-                
+            <Stack direction='horizontal' gap={2} className='justify-content-end'>
+                <Button type='submit' variant='primary'>Save</Button>
+                <Link to='..'>
+                    <Button type='button' variant='outline-secondary'>Cancel</Button>
+                </Link>
             </Stack>
         </Stack>
     </Form>
