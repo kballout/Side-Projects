d013849b9dbc4dd3e581a171f1b4ac1149af1dcb
Kassim Ballout <54900156+kballout@users.noreply.github.com>
2022-12-02 21:49:55 -0500

    added product component

---
+++ ecommerce-app/components/Item.js
@@ -0,0 +1,25 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from 'next/link'
+import React from 'react'
+
+export default function Item({product}) {
+  return (
+    <div className='card'>
+        <Link href={`product/${product.slug}`}>
+            <img
+                src={product.image}
+                alt={product.name}
+            />
+        </Link>
+
+        <div className='flex flex-col justify-center items-center p-5'>
+            <Link href={`product/${product.slug}`}>
+                <h2 className='text-lg'>{product.name}</h2>
+            </Link>
+            <p className='mb-2'>{product.brand}</p>
+            <p>${product.price}</p>
+            <button className='cartButton' type='button'>Add to cart</button>
+        </div>
+    </div>
+  )
+}
--- ecommerce-app/pages/index.js
+++ ecommerce-app/pages/index.js
@@ -1,9 +1,17 @@
 import Layout from "../components/Layout";
+import Item from "../components/Item";
+import data from "../utils/testdata";
 
 export default function Home() {
   return (
     <Layout title={'Home Page'}>
-      Home
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => {
+          return(
+            <Item product={product} key={product.slug}/>
+          )
+        })}
+      </div>
     </Layout>
   )
 }
