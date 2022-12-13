ddf296504527333aae8620e23bc665ba93871c85
Kassim Ballout <54900156+kballout@users.noreply.github.com>
2022-12-13 16:35:00 -0500

    fixed file name

--- ecommerce-app/pages/cart.js
+++ ecommerce-app/pages/cart.js
@@ -101,7 +101,7 @@ function CartScreen() {
               </li>
               <li>
                 <button
-                  onClick={() => router.push("login?redirect=/Shipping")}
+                  onClick={() => router.push("login?redirect=/shipping")}
                   className="w-full cartButton"
                 >
                   Check Out
