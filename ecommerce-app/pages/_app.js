93748407c9729191d91287581b7e6963300996c4
Kassim Ballout <54900156+kballout@users.noreply.github.com>
2022-12-16 15:05:22 -0500

    Integration of paypal

--- ecommerce-app/pages/_app.js
+++ ecommerce-app/pages/_app.js
@@ -1,37 +1,40 @@
-import '../styles/globals.css'
-import { Store } from '../utils/Store'
-import {Provider} from "react-redux"
-import { SessionProvider, useSession } from 'next-auth/react'
-import { useRouter } from 'next/router'
+import "../styles/globals.css";
+import { Store } from "../utils/Store";
+import { Provider } from "react-redux";
+import { SessionProvider, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
-function MyApp({ Component, pageProps: {session, ...pageProps} }) {
-  return(
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return (
     <SessionProvider session={session}>
       <Provider store={Store}>
-        {Component.auth ? (
-          <Auth>
-            <Component {...pageProps}/>
-          </Auth>
-        ): (
-          <Component {...pageProps} />
-        )}
+        <PayPalScriptProvider deferLoading={true}>
+          {Component.auth ? (
+            <Auth>
+              <Component {...pageProps} />
+            </Auth>
+          ) : (
+            <Component {...pageProps} />
+          )}
+        </PayPalScriptProvider>
       </Provider>
     </SessionProvider>
-  ) 
+  );
 }
 
-function Auth({children}) {
-  const router = useRouter()
-  const {status} = useSession({
+function Auth({ children }) {
+  const router = useRouter();
+  const { status } = useSession({
     required: true,
-    onUnauthenticated(){
-      router.push('/unauthorized?message=login required')
-    }
-  })
-  if(status === 'loading') {
-    return <div>Loading...</div>
+    onUnauthenticated() {
+      router.push("/unauthorized?message=login required");
+    },
+  });
+  if (status === "loading") {
+    return <div>Loading...</div>;
   }
-  return children
+  return children;
 }
 
-export default MyApp
+export default MyApp;
